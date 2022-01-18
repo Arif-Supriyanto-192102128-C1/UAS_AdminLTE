2c76378cc0a20cd87cbeffc1a9487e068f19a5b8
ArifSupriyanto192102128 <arifsupriyanto373@gmail.com>
2022-01-18 15:23:00 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DivisionComponent } from './division/division.component';
+import { TambahDataComponent } from './tambah-data/tambah-data.component';
+import { EditDataComponent } from './edit-data/edit-data.component';
+import { HapusDataComponent } from './hapus-data/hapus-data.component';
 
 const routes: Routes = [
   {path:"dashboard",component:DashboardComponent},
-  {path:"divisi",component:DivisionComponent}
+  {path:"divisi",component:DivisionComponent},
+  {path:"tambah",component:TambahDataComponent},
+  {path:"edit",component:EditDataComponent},
+  {path:"hapus", component:HapusDataComponent}
 ];
 
 @NgModule({
